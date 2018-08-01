a0b93f3f12e79e3beda990e849c7f0deebaaccfe
testbird <noreply@noreply.github.com>
2018-08-01 17:42:50 +0200

    stop point testing

--- install.sh
+++ install.sh
@@ -45,10 +45,46 @@ echo -e "\nInstalling NextCloudPi"
 source etc/library.sh
 
 install_script  lamp.sh
+
+echo "Ready to install NC"
+read -p "Are you sure? [y/n]" -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  exit 1
+fi
+
 install_script  etc/ncp-config.d/nc-nextcloud.sh
+
+echo "Ready to configure NC"
+read -p "Are you sure? [y/n]" -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  exit 1
+fi
 activate_script etc/ncp-config.d/nc-nextcloud.sh
+
+echo "Ready to install NCP-web"
+read -p "Are you sure? [y/n]" -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  exit 1
+fi
 install_script  ncp.sh
+
+echo "Ready to configure NCP-web"
+read -p "Are you sure? [y/n]" -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  exit 1
+fi
 activate_script etc/ncp-config.d/nc-init.sh
+
+echo "Ready to install post-inst.sh"
+read -p "Are you sure? [y/n]" -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  exit 1
+fi
 [[ -f /.ncp-image ]] && install_script post-inst.sh
 
 popd
